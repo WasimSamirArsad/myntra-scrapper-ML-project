1e367fb139599c5431e333a52114dbdd6de25d56
WasimSamirArsad <wasimakhtar12091@gmail.com>
2024-09-29 20:58:24 +0530

    setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup ,find_packages
+
+
+setup(
+    name="scrapper",
+    version="0.0.1",
+    author="wasim akhtar",
+    packages=find_packages(),
+    author_email="wasimcode12091@gmail.com",
+    install_requires=[]
+)
